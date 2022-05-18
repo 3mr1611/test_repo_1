8d029fefa948a4e0ff5fcf2ed7b03222b2732f82
AMRDARWISH <amrdarwish1199@gmail.com>
2022-05-18 16:18:08 +0200

    first commit

---
+++ 01-DIO-Basic/main.c
@@ -0,0 +1,28 @@
+/*
+ * main.c
+
+ *
+ *  Created on: May 18, 2022
+ *      Author: 3mr darwish
+ */
+#include <avr/io.h>
+#include<util/delay.h>
+int main(void){
+DDRA=0b00001111; //PORTA0 output
+
+while(1)//super loop
+{
+	//PORTA=0b00000001; //PA0 HIGH(5v)
+	//_delay_ms(1000);
+	//PORTA=0; //PA0 low
+	//_delay_ms(1000);
+	PORTA=0b00000001;
+	for(int i=0;i<4;i++){
+
+		_delay_ms(250);
+		PORTA <<=2;
+	}
+}
+}
+
+
